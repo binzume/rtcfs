54607bac379e7f65bf5cfedcb42197af6314c35c
Kousuke Kawahira <kawahira@binzume.net>
2023-12-29 21:25:49 +0900

    add mimeType for .mov

--- socfs/file_handler.go
+++ socfs/file_handler.go
@@ -135,6 +135,7 @@ var ContentTypes = map[string]string{
 	".mp4":  "video/mp4",
 	".m4v":  "video/mp4",
 	".f4v":  "video/mp4",
+	".mov":  "video/mp4",
 	".webm": "video/webm",
 	".ogv":  "video/ogv",
 
